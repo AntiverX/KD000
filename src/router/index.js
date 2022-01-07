0a89b2edee93485df43ae4bc8293a9729bd8d947
Shuaipeng Wang <Antiver@foxmail.com>
2022-01-07 21:46:12 +0800

    modify menu

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,10 @@ Vue.use(Router)
 import Layout from '@/layout'
 
 /* Router Modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
+// import componentsRouter from './modules/components'
+// import chartsRouter from './modules/charts'
 import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
+// import nestedRouter from './modules/nested'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -416,7 +416,7 @@ export const asyncRoutes = [
   // componentsRouter,
   // chartsRouter,
   // nestedRouter,
-  // tableRouter,
+  tableRouter,
   //
   // {
   //   path: '/example',
